88fa8bb273b249e8c35b51cac1074048481c2dc4
Martti Malmi <sirius@iki.fi>
2022-03-24 15:59:07 +0200

    gunjs service as www-data

--- install_gunjs.sh
+++ install_gunjs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release nvm install 16
 node_path=$(which node)
 
@@ -18,6 +21,7 @@ After=network.target
 Type=forking
 ExecStart=$node_path /home/pi/src/gun-js/examples/http.js
 Restart=on-failure
+User=www-data
 
 [Install]
 WantedBy=multi-user.target\" > /lib/systemd/system/gunjs.service"
