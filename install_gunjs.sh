b9b08321ebd03e99059e44d04b5058e8b294718f
Martti Malmi <sirius@iki.fi>
2022-03-30 20:40:40 +0300

    select different nodejs on armv6

--- install_gunjs.sh
+++ install_gunjs.sh
@@ -3,9 +3,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-# TODO: install 16 if armv6 (pi zero), otherwise stable without mirror
-NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release nvm install 16
-nvm install stable
+if [[ $(uname -a) == *armv6* ]]; then
+	NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release nvm install 16
+else
+	nvm install stable
+fi
 node_path=$(which node)
 
 mkdir /home/pi/src
