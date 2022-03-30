d71f097912ecd4c9efa945afdcd323a9b72bab4e
Martti Malmi <sirius@iki.fi>
2022-03-30 14:28:20 +0300

    add public/chat and nginx websocket proxy

--- install_gunjs.sh
+++ install_gunjs.sh
@@ -5,7 +5,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 # TODO: install 16 if armv6 (pi zero), otherwise stable without mirror
 NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release nvm install 16
-NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release nvm install stable
+nvm install stable
 node_path=$(which node)
 
 mkdir /home/pi/src
