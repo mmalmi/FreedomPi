a287ab4971d842dbb2576787ba4d9e46d02edece
Martti Malmi <sirius@iki.fi>
2022-03-31 22:19:00 +0300

    fm_transmitter comment

--- radio/fm_transmitter_play.sh
+++ radio/fm_transmitter_play.sh
@@ -7,7 +7,7 @@ do
 		if [[ $file == *".wav" ]]; then
 			sudo /home/pi/src/fm_transmitter/fm_transmitter -f 88.3 $file || true
 		else
-			sox "$file" -r 22050 -c 1 -b 16 -t wav - | sudo /home/pi/src/fm_transmitter/fm_transmitter -f 88.3 - || true
+			sox "$file" -r 22050 -c 1 -b 16 -t wav - | sudo /home/pi/src/fm_transmitter/fm_transmitter -f 88.3 - || true # todo: fm_transmitter doesn't terminate after stdin ends. use another software or fix.
 		fi
 	done
 	sleep 1
