c16e48a89452d9512863519f0aaecdaa98d4b14c
Martti Malmi <sirius@iki.fi>
2022-03-31 22:05:21 +0300

    filesharing setup, index.html

--- install_nginx.sh
+++ install_nginx.sh
@@ -2,4 +2,4 @@ apt install php7.4-fpm nginx -y
 systemctl enable php7.4-fpm
 cp /home/pi/FreedomPi/config/nginx-site.conf /etc/nginx/sites-enabled/default
 service nginx restart
-chmod a+rw /home/pi/FreedomPi/share/files
+chmod -R a+rw /home/pi/FreedomPi/public/share/files
