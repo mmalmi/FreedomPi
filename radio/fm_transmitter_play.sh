c9aa73fc9ac1a70f5618c8a760d37c5091354e92
Martti Malmi <sirius@iki.fi>
2022-03-31 14:39:10 +0300

    radio: mp3 and other formats supported by sox

--- radio/fm_transmitter_play.sh
+++ radio/fm_transmitter_play.sh
@@ -3,8 +3,11 @@ while true
 do
 	for file in /home/pi/FreedomPi/radio/audio/*
 	do
-		sudo /home/pi/src/fm_transmitter/fm_transmitter -f 90.0 $file
-		# sox "$file" -r 22050 -c 1 -b 16 -t wav - | sudo /home/pi/src/fm_transmitter/fm_transmitter -f 90 -
+		if [[ $file == *".wav" ]]; then
+			sudo /home/pi/src/fm_transmitter/fm_transmitter -f 90.0 $file
+		else
+			sox "$file" -r 22050 -c 1 -b 16 -t wav - | sudo /home/pi/src/fm_transmitter/fm_transmitter -f 90 -
+		fi
 	done
 	sleep 1 # you can remove this if you added files into the audio directory
 done
